63f1840b37dee1ed27ebada0b1e9f326ebd708b1
veeraprathap <veeraprathap.m@comakeit.com>
2018-10-09 16:50:22 +0530

    done

--- src/test/java/com/GetInputsFromJenkins/GetInputsFromJenkins.java
+++ src/test/java/com/GetInputsFromJenkins/GetInputsFromJenkins.java
@@ -2,12 +2,33 @@ package com.GetInputsFromJenkins;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.testng.TestNG;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.apache.tools.ant.util.ClasspathUtils;
 import org.testng.annotations.Test;
 
 public class GetInputsFromJenkins {
+	
+	List alltestcases=new ArrayList();
 	
 	@Test
 	
@@ -50,7 +71,7 @@ public class GetInputsFromJenkins {
 		
 		for (File file : allpackages) {
 			
-			System.out.println(file);
+			System.out.println(file.getName());
 			
 		}
 		
@@ -64,5 +85,71 @@ public class GetInputsFromJenkins {
 	
 	
 	}
+	
+	
+	
+	/*public static void buildXML()
+	{
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+		// root elements
+				Document doc = docBuilder.newDocument();
+				Element rootElement = doc.createElement("suite");
+				doc.appendChild(rootElement);
+		
+		// set attribute to root element
+				Attr attr = doc.createAttribute("name");
+				attr.setValue("Suite of Suits");
+				rootElement .setAttributeNode(attr);
+				
+				
+	   // Test
+				Element test = doc.createElement("test");
+				rootElement.appendChild(test);		
+				
+		// set attribute to Test element
+				Attr testattr = doc.createAttribute("name");
+				testattr.setValue("Test");
+				test .setAttributeNode(testattr);
+				
+				
+		// Classes
+				Element classes= doc.createElement("classes");
+				test.appendChild(classes);	
+				
+				//Class
+				
+				for(int i=0;i<=alltestcases.size()-1;i++)
+				{
+					
+					//Class
+					Element classinsideclasses= doc.createElement("class");
+					classes.appendChild(classinsideclasses);
+					
+					// set attribute to Class element
+					Attr classattr = doc.createAttribute("name");	
+					
+				classattr.setValue("com.TestCases."+alltestcases.get(i).toString());
+				classinsideclasses .setAttributeNode(classattr);	
+				}
+				
+				
+		// write the content into xml file
+				TransformerFactory transformerFactory = TransformerFactory.newInstance();
+				Transformer transformer = transformerFactory.newTransformer();
+				DOMSource source = new DOMSource(doc);
+				StreamResult result = new StreamResult(new File(Homepath+"\\TestCases.xml"));
+
+				// Output to console for testing
+				// StreamResult result = new StreamResult(System.out);
+
+				transformer.transform(source, result);
+
+				System.out.println("File saved!");	
+	}
+	
+*/	
+	
 
 }
