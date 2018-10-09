7080ab9ed22b75f6b6af52d273b454fa7790ce9a
veeraprathap <veeraprathap.m@comakeit.com>
2018-10-09 12:28:55 +0530

    ChangedInput

--- src/test/java/GetInputsFromJenkins/GetInputsFromJenkins.java
+++ src/test/java/GetInputsFromJenkins/GetInputsFromJenkins.java
@@ -8,7 +8,7 @@ public class GetInputsFromJenkins {
 	
 	public static void getInputsFromJenkins()
 	{
-		System.out.println(System.getenv("Modulename"));
+		System.out.println(System.getenv("Modules"));
 	}
 
 }
