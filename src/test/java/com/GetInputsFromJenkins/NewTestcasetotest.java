09d94f8c1312f575f9a39cc46fffae388aa04f63
veeraprathap <veeraprathap.m@comakeit.com>
2018-10-09 14:34:54 +0530

    test

---
+++ src/test/java/com/GetInputsFromJenkins/NewTestcasetotest.java
@@ -0,0 +1,28 @@
+package com.GetInputsFromJenkins;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.TestNG;
+import org.testng.annotations.Test;
+import org.testng.xml.XmlClass;
+import org.testng.xml.XmlSuite;
+import org.testng.xml.XmlTest;
+
+public class NewTestcasetotest {
+	
+	@Test
+	
+	public static void getInputsFromJenkins()
+	{
+		int i=1;
+		while(i<=15)
+		{
+			System.out.println("running "+i);
+			i++;
+		}
+		
+		
+		
+	}
+}
